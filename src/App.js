6e1bba145e9b6e40b9dd9a8079b0d6a9254b9d43
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-07-17 12:25:50 +0700

    uploap project tiktok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [info, setInfo] = useState({
+    name: "Trần Đình Huy",
+    age: 18,
+    address: "Bình Dương , Việt Nam",
+  });
+
+  const handleUpdate = () => {
+    setInfo({
+      ...info,
+      description: "Hello ae ^^ tôi là người yêu màu hồng và ghét sự giả dối!",
+    });
+  };
+
   return (
-    <div className="App">
-      <h2>Hello React</h2>
+    <div className="center">
+      <h1>React App</h1>
+      <p>{JSON.stringify(info)}</p>
+      <button onClick={handleUpdate}>Update Profile User</button>
     </div>
   );
 }
