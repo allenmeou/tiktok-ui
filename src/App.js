995589ebb24f2da9cd4efc057183fe41b58e4cb6
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-07-18 16:09:57 +0700

    uploap project tiktok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const storageJobs = JSON.parse(localStorage.getItem("job"));
+
+  const [jobs, setJobs] = useState(storageJobs);
   const [job, setJob] = useState("");
-  const [jobs, setJobs] = useState([]);
+
+  const inputRef = useRef();
 
   const handleSubmit = () => {
-    setJobs((prev) => [...prev, job]);
+    setJobs((prev) => {
+      const newJobs = [...prev, job];
+
+      const jsonJobs = JSON.stringify(newJobs);
+      localStorage.setItem("job", jsonJobs);
+
+      return newJobs;
+    });
     setJob("");
+    inputRef.current.focus();
   };
 
   return (
     <div style={{ padding: 200 }}>
       <input
+        ref={inputRef}
         value={job}
         type="text"
+        placeholder="Nhập việc bàn muốn làm"
         onChange={(e) => {
           setJob(e.target.value);
         }}
       />
-      <button onClick={handleSubmit}> Add </button>
+      <button onClick={handleSubmit}>Add</button>
 
       <ul>
         {jobs.map((job, index) => (
