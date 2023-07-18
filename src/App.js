08f7df52e447ebf70e72239afffefd66e090819c
Tran Dinh Huy <104847278+allenmeou@users.noreply.github.com>
2023-07-18 16:47:12 +0700

    uploap project tiktok

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const storageJobs = JSON.parse(localStorage.getItem("job"));
-
-  const [jobs, setJobs] = useState(storageJobs ?? []);
   const [job, setJob] = useState("");
+
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem("job")) || [];
+    return storageJobs;
+  });
 
   const inputRef = useRef();
 
@@ -22,13 +24,26 @@ function App() {
     inputRef.current.focus();
   };
 
+  const handleEdit = (job) => {
+    console.log(job);
+    const liElement = document.querySelector("li");
+    console.log(liElement);
+    liElement.innerHTML = "";
+    setJob(job);
+  };
+
+  const handleDelete = () => {
+    console.log("handleDelete");
+  };
+
   return (
     <div style={{ padding: 200 }}>
+      <h2>To do list </h2>
       <input
         ref={inputRef}
         value={job}
         type="text"
-        placeholder="Nhập việc bàn muốn làm"
+        placeholder="new task"
         onChange={(e) => {
           setJob(e.target.value);
         }}
@@ -38,7 +53,12 @@ function App() {
 
       <ul>
         {jobs.map((job, index) => (
-          <li key={index}>{job}</li>
+          <div className="app" key={index}>
+            <input type="checkbox" name="" id="" />
+            <li>{job}</li>
+            <button onClick={() => handleEdit(job)}>edit</button>
+            <button onClick={handleDelete}>delete</button>
+          </div>
         ))}
       </ul>
     </div>
